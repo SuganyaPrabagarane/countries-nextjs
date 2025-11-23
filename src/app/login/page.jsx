e36d67da0e562d528ecfaa96b0af6d4f78727094
Suganya Prabagarane <suganyaprabagarane@gmai.com>
2025-11-23 21:38:43 +0200

    remove deployed app url

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -53,7 +53,6 @@ const Login = () => {
                 providers={["google"]}
                 socialLayout="horizontal"
                 view="sign_in"
-                redirectTo={process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"}
                 />
             </Paper>
             </Box>
